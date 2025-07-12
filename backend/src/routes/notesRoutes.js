32784d02945e743bf25769a1c60430509a70f890
pikachu <msram2006@gmail.com>
2025-07-12 22:28:00 +0530

    Initialize backend with Express, MongoDB, and basic note management API
    - Created package.json for backend dependencies and scripts
    - Implemented MongoDB connection in db.js
    - Developed notes controller with CRUD operations
    - Defined Note model with Mongoose schema
    - Set up routes for notes API
    - Configured server to use notes routes and connect to the database

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,14 @@
+import express from  'express';
+import { createNote, deleteNote, getAllNotes, updateNote } from '../controllers/notesController.js';
+
+const router = express.Router();
+
+
+router.get("/", getAllNotes);
+router.post("/" ,createNote);
+router.post("/:id" ,updateNote);
+router.delete("/:id",deleteNote); 
+
+
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import notesRoutes from "./routes/notesRoutes.js";
+import {connectDB} from './config/db.js';
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5001
+connectDB();
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on port : PORT");
+})
+
+app.use("/api/notes" , notesRoutes);
+
+
